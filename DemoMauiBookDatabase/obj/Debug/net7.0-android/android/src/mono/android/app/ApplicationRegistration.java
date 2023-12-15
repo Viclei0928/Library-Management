1a2f71fb7015aa1d0578c87458c757484b6432cd
Viclei0928 <141261894+Viclei0928@users.noreply.github.com>
2023-12-15 14:24:30 -0700

    System Code

---
+++ DemoMauiBookDatabase/obj/Debug/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("DemoMauiBookDatabase.MainApplication, DemoMauiBookDatabase, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64389b793217dabe89.MainApplication.class, crc64389b793217dabe89.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
